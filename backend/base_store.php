af606057469dc1112c88fde3b4b9c8855df2b82b
st4rossz <kanit.je@rmuti.ac.th>
2021-12-02 12:31:00 +0700

    taro process4_1

--- backend/base_store.php
+++ backend/base_store.php
@@ -22,12 +22,13 @@ include 'layout/header.php';
               <!-- <div class="header"> -->
               <h4 class="title">เพิ่มข้อมูลร้าน </h4>
                 <div class="d-flex">
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Open modal for @mdo</button>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">เพิ่ม</button>
+              
                 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+        <h5 class="modal-title" id="exampleModalLabel">โปรดกรอกข้อมูลร้าน</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -35,23 +36,22 @@ include 'layout/header.php';
       <div class="modal-body">
         <form>
           <div class="form-group">
-            <label for="recipient-name" class="col-form-label">Recipient:</label>
+            <label for="recipient-name" class="col-form-label">ชื่อ : </label>
             <input type="text" class="form-control" id="recipient-name">
           </div>
           <div class="form-group">
-            <label for="message-text" class="col-form-label">Message:</label>
+            <label for="message-text" class="col-form-label">ที่อยู่ : </label>
             <textarea class="form-control" id="message-text"></textarea>
           </div>
         </form>
       </div>
-      <div class="modal-footer"></div>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Send message</button>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">ปิด</button>
+        <button type="button" class="btn btn-primary">เพิ่ม</button>
       </div>
     </div>
   </div>
 </div>
-                  
                 </div>
               <hr>
               <!-- <p class="category">Here is a subtitle for this table</p> -->
